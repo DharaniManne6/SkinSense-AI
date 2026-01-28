1c39e2929415b79a273e47e2299aced77e4ee8a8
DharaniManne6 <dharu.manne@gmail.com>
2026-01-28 00:51:03 -0500

    changes made app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def recommend():
         concern=concern,
         ingredients=recommendations
     )
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
